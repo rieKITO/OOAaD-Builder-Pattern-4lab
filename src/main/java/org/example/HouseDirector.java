a395933ca9f4077c31bc67f01d417357d800891f
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-05-03 02:24:18 +0300

    added spaces

--- src/main/java/org/example/HouseDirector.java
+++ src/main/java/org/example/HouseDirector.java
@@ -2,11 +2,13 @@ package org.example;
 
 public class HouseDirector {
     HouseBuilder builder;
+
     public HouseDirector(HouseBuilder builder) {
         this.builder = builder;
         if (this.builder == null)
             throw new IllegalArgumentException("House director can't work without house builder!");
     }
+
     public House construct(double area, int floors, String materials, boolean hasGarden) {
         builder.buildArea(area);
         builder.buildFloors(floors);
